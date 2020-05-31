311556c55890c5c74267d056d06db78f4a22639e
akuakuka <aku.uusikolli@gmail.com>
2020-05-31 13:34:25 +0300

    kaanto

---
+++ BuildScript.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const fse = require("fs-extra");
+const childProcess = require("child_process");
+
+if (fs.existsSync("./build")) {
+  fse.removeSync("./build");
+}
+
+if (fs.existsSync("./dist")) {
+  fse.removeSync("./dist");
+}
+
+// childProcess.execSync("react-scripts build", { stdio: "inherit" });
+ childProcess.execSync("tsc -p server", { stdio: "inherit" });
+
+
+fse.moveSync("./build", "./dist/app/public", { overwrite: true });
+
--- src/state/thunks/pluginThunk.ts
+++ src/state/thunks/pluginThunk.ts
@@ -7,9 +7,6 @@ import { setTelegramVerify } from '../slices/pluginSlice';
 
 export type PluginThunk = ThunkAction<void, RootState, null, Action<string>>;
 
-
-
-
 export const getVerifyCode = (plugin:string): PluginThunk => async (dispatch) => {
 try {
 const response = await axios.get(`${backendURL}/user/verify/${plugin}`)
