05886ef2159d0fbfb46d090e57a63c26f28c5dca
akuakuka <aku.uusikolli@gmail.com>
2020-05-31 19:47:21 +0300

    frontendurlsetup

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,12 @@
-export const backendURL = 'http://localhost:3000'
+
+export const getBackEndUrl = () => {
+    let backendURL;
+    if (process.env.NODE_ENV === "production") {
+        backendURL = 'https://superdupertaskmemolist.herokuapp.com'
+       
+    } else (process.env.NODE_ENV === "development") {
+        backendURL = "http://localhost:3000"
+    } 
+    return backendURL
+}
+
